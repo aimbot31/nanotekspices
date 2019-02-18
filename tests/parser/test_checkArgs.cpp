433c7b8c851f39255bc18e166b9700f85b5928fc
JOK3R <aimbot31@gmail.com>
2019-02-19 00:49:39 +0100

    [ADD] More unit tests

--- tests/parser/test_checkArgs.cpp
+++ tests/parser/test_checkArgs.cpp
@@ -26,8 +26,20 @@ Test(checkArgs_bad_3, checkArgs)
 	cr_assert_any_throw(checkArgs(1, str));
 }
 
+Test(checkArgs_bad_4, checkArgs)
+{
+	char *str[4] = {"./test", "./tests/and.nts", "a=1", "b=2"};
+	cr_assert_any_throw(checkArgs(1, str));
+}
+
 Test(checkArgs_good, checkArgs)
 {
 	char *str[] = {"./test", "./tests/and.nts", "a=0", "b=0"};
+	cr_assert_none_throw(checkArgs(4, str));
+}
+
+Test(checkArgs_good_2, checkArgs)
+{
+	char *str[] = {"./test", "./tests/and.nts", "a=1", "b=1"};
 	cr_assert_none_throw(checkArgs(4, str));
 }
