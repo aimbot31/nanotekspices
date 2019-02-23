7d094edd3e7e2c5dadb6eba9d5ee847e52101b2d
aimbot31 <florian.davasse@epitech.eu>
2019-02-23 19:58:49 +0100

    [FIX] printing style

--- src/NanoTekSpice.cpp
+++ src/NanoTekSpice.cpp
@@ -5,7 +5,6 @@
 ** NanoTekSpice
 */
 
-#include "signal.h"
 #include "NanoTekSpice.hpp"
 
 using namespace nts;
@@ -15,64 +14,64 @@ bool NanoTekSpice::_Running = false;
 NanoTekSpice::NanoTekSpice(int ac, char **av, const std::string &name) :
 nts::Circuit(ac, av, name)
 {
-    _Running = false;
+	_Running = false;
 }
 
 std::string &NanoTekSpice::getNextCommand(void)
 {
-	std::cout << ">";
+	std::cout << "> ";
 	getline(std::cin, _CurrentCommand);
-    return _CurrentCommand;
+	return _CurrentCommand;
 }
 
 void NanoTekSpice::updateInputFromCurrentCommand(void)
 {
 		std::size_t pos = _CurrentCommand.find('=');
 		if (pos == std::string::npos || pos == _CurrentCommand.length()) {
-            std::cout << "Invalid Command" << std::endl;
-            return;
-        }
+			std::cout << "Invalid Command" << std::endl;
+			return;
+		}
 
-        std::string name = _CurrentCommand.substr(0, pos);
-        int value = std::stoi(_CurrentCommand.substr(pos+1, _CurrentCommand.length()), nullptr, 10);
-        if (value != 0 && value != 1) {
-            std::cout << "Invalid Value" << std::endl;
-            return;
-        }
+		std::string name = _CurrentCommand.substr(0, pos);
+		int value = std::stoi(_CurrentCommand.substr(pos+1, _CurrentCommand.length()), nullptr, 10);
+		if (value != 0 && value != 1) {
+			std::cout << "Invalid Value" << std::endl;
+			return;
+		}
 
-	    setInputValue(name, nts::Tristate(value));
+		setInputValue(name, nts::Tristate(value));
 }
 
 void NanoTekSpice::run(void)
 {
-    while (getNextCommand().length() != 0 && _CurrentCommand != "EOF")
-        DoCommandAction();
+	while (getNextCommand().length() != 0 && _CurrentCommand != "EOF")
+		DoCommandAction();
 }
 
 void NanoTekSpice::stopRunning(int i)
 {
-    (void)i;
-    _Running = false;
+	(void)i;
+	_Running = false;
 }
 
 void NanoTekSpice::doNothing(int i)
 {
-    (void)i;
+	(void)i;
 }
 
 void NanoTekSpice::simulateInLoop(void)
 {
-    _Running = true;
-    signal(SIGINT, stopRunning);
-    while (nts::NanoTekSpice::_Running)
-        computeAll();
-    signal(SIGINT, doNothing);
+	_Running = true;
+	std::signal(SIGINT, stopRunning);
+	while (nts::NanoTekSpice::_Running)
+		computeAll();
+	std::signal(SIGINT, doNothing);
 }
 
 
 void NanoTekSpice::endRun(void)
 {
-    exit(0);
+	exit(0);
 }
 
 void NanoTekSpice::DoCommandAction(void)
@@ -86,8 +85,8 @@ void NanoTekSpice::DoCommandAction(void)
 
 	if (tab.count(_CurrentCommand) == 1) {
 		((*this).*tab[_CurrentCommand])();
-    } else
-        updateInputFromCurrentCommand();
+	} else
+		updateInputFromCurrentCommand();
 }
 
 
