d426a0fb6ec55a731303f64be090e1de962b35ab
JOK3R <aimbot31@gmail.com>
2019-02-24 19:15:59 +0100

    [ADD] Exceptions

--- src/exceptions/Exceptions.cpp
+++ src/exceptions/Exceptions.cpp
@@ -26,3 +26,27 @@ nts::InputError::InputError(const std::string &msg, const std::string &component
 	_msg = "Input error : " + msg;
 	_component = component;
 }
+
+nts::ComponentError::ComponentError(const std::string &msg, const std::string &component)
+{
+	_msg = "Component error : " + msg;
+	_component = component;
+}
+
+nts::PinError::PinError(const std::string &msg, const std::string &component)
+{
+	_msg = "Pin error : " + msg;
+	_component = component;
+}
+
+nts::LinkError::LinkError(const std::string &msg, const std::string &component)
+{
+	_msg = "Link error : " + msg;
+	_component = component;
+}
+
+nts::FileError::FileError(const std::string &msg, const std::string &component)
+{
+	_msg = "File error : " + msg;
+	_component = component;
+}
